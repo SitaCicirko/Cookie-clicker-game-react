c0343795bf5edede8f1c64a3bde9201029dcb4e1
SitaCicirko <ns.cicirko@gmail.com>
2025-01-26 23:26:58 +0000

    its awesome

--- src/components/Upgrades.jsx
+++ src/components/Upgrades.jsx
@@ -3,33 +3,32 @@
 // and the only thing that came up was above code. Ive read the files and I didnt understand them :D
 
 export default function Upgrades(props) {
-  function buyMagicFlour() {
-    if (props.cookieCount >= 50) {
-      props.setCookieCount(props.cookieCount - 50);
-      props.setMessage("Magic Flour purchased!");
-    } else {
-      props.setMessage("Not enough cookies for Magic Flour!");
-    }
-  }
+  const upgrades = [
+    { name: "Magic Flour", cost: 50 },
+    { name: "Special Chocolate", cost: 100 },
+  ];
 
-  function buySpecialChocolate() {
-    if (props.cookieCount >= 100) {
-      props.setCookieCount(props.cookieCount - 100);
-      props.setMessage("Special Chocolate purchased!");
+  function buyUpgrade(name, cost) {
+    if (props.cookieCount >= cost) {
+      props.setCookieCount(props.cookieCount - cost);
+      props.setMessage(`${name} purchased!`);
     } else {
-      props.setMessage("Not enough cookies for Special Chocolate!");
+      props.setMessage(`Not enough cookies for ${name}!`);
     }
   }
 
   return (
     <div>
       <h2>Upgrades</h2>
-      <button className="upgrade-button" onClick={buyMagicFlour}>
-        Buy Magic Flour (50 Cookies)
-      </button>
-      <button className="upgrade-button" onClick={buySpecialChocolate}>
-        Buy Special Chocolate (100 Cookies)
-      </button>
+      {upgrades.map((upgrade) => (
+        <button
+          key={upgrade.name}
+          className="upgrade-button"
+          onClick={() => buyUpgrade(upgrade.name, upgrade.cost)}
+        >
+          Buy {upgrade.name} ({upgrade.cost} Cookies)
+        </button>
+      ))}
     </div>
   );
 }
